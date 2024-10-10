0f79b5b5f2da70b71e20368dea28ab5acfb933c0
UwUBabushkaUwU <chinar120605@gmail.com>
2024-10-10 12:42:13 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chinarrm23/hackathon_ws/src
+# Build directory: /home/chinarrm23/hackathon_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("manipulator")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/chinarrm23/hackathon_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/chinarrm23/hackathon_ws/devel;/opt/ros/noetic'.split(';') if '/home/chinarrm23/hackathon_ws/devel;/opt/ros/noetic' != '' else []
