5d29f72248fb142c480aa051c603a5f286eb51ff
UwUBabushkaUwU <chinar120605@gmail.com>
2024-10-14 22:33:32 +0530

    final commit

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("movit_manipulator_sim")
 subdirs("fanipulator")
 subdirs("manipulator")
---
+++ build/movit_manipulator_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chinarrm23/hackathon_ws/src/movit_manipulator_sim
+# Build directory: /home/chinarrm23/hackathon_ws/build/movit_manipulator_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
